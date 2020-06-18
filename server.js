367730c312756fd137b73089172300a860901c05
Eda Tuncer <edatezcanli@gmail.com>
2020-06-18 13:27:28 -0700

    updated the files

--- server.js
+++ server.js
@@ -1,36 +1,31 @@
-const express =require ("express");
-const path =require ("path");
+const express = require("express");
+const path = require("path");
 //const { Console } = require("console");
 //const apiRoutes = require("./Routes/apiRoutes.js")
-var PORT = process.env.PORT || 3000;
-
-const app= express();
+// var PORT = process.env.PORT || 3000;
+var PORT = 3000;
+const app = express();
 
 // Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
 
-app.use(express.static ('public'));
-
-
-//te
 //app.use(apiRoutes);
-require("./Routes/apiRoutes")(app)
+require("./Routes/apiRoutes")(app);
 
 app.get("/notes", (req, res) => {
-    console.log("/notes")
+  console.log("/notes");
 
-   res.sendFile (path.join (__dirname, './public/notes.html') );
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("/", (req, res) => {
-    console.log("/ index")
-    res.sendFile (path.join (__dirname, './public/index.html') );
+  console.log("/ index");
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-
 
 app.listen(PORT, () => {
-    console.log (`Listening to port: ${PORT}`);
+  console.log(`Listening to port: ${PORT}`);
 });
-
